d5f91b9dce451db82416caab5f9e1eb088644fc8
Nazam <nazamfr1998@gmail.com>
2022-02-14 12:46:51 +0100

    added Seeder Statut Type and Utility

--- Modules/Inventaire/Database/Seeders/InventaireDatabaseSeeder.php
+++ Modules/Inventaire/Database/Seeders/InventaireDatabaseSeeder.php
@@ -16,6 +16,6 @@ class InventaireDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(["StatutSeederTableSeeder", "TypeSeederTableSeeder", "UtilitySeederTableSeeder"]);
     }
 }
