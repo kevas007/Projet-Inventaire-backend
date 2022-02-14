007639fec749c9daac0c32b5dd7f0e83f1353aac
kevas007 <kevassiobo@gmail.com>
2022-02-14 09:28:48 +0100

    ajout du module inventaire

---
+++ Modules/Inventaire/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('inventaire::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('inventaire.name') !!}
+    </p>
+@endsection
