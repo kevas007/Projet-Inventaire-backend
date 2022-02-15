2e6e00e60afe0832f838fb6250cd26741ca02731
kevas007 <kevassiobo@gmail.com>
2022-02-15 10:11:06 +0100

    installation de vuejs

---
+++ Modules/Inventaire/Resources/assets/components/Inventaire.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Inventaire</h1>
+        <h2>Liste des produits</h2>
+        <p>
+                <button class="btn btn-primary">Ajouter un produit</button>
+            </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Inventaire'
+}
+</script>
--- Modules/Inventaire/Resources/views/index.blade.php
+++ Modules/Inventaire/Resources/views/index.blade.php
@@ -6,4 +6,6 @@
     <p>
         This view is loaded from module: {!! config('inventaire.name') !!}
     </p>
-@endsection
+
+    <inventaire></inventaire>
+    @endsection
