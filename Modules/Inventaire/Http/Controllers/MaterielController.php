ff7bbce39e94c3bae6918c03afb33281e4e37b17
Nazam <nazamfr1998@gmail.com>
2022-02-15 17:59:41 +0100

    added materiel ajout

--- Modules/Inventaire/Http/Controllers/MaterielController.php
+++ Modules/Inventaire/Http/Controllers/MaterielController.php
@@ -5,6 +5,7 @@ namespace Modules\Inventaire\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Inventaire\Entities\Materiel;
 
 class MaterielController extends Controller
 {
@@ -34,10 +35,28 @@ class MaterielController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'nom'=>'required|min:1',
-            'numero_serie'=>'required|min:1',
-            'token'=>'required|min:1',
+            'nom' => 'required|min:1',
+            'numero_serie' => 'required|min:1',
+            'token' => 'required|min:1',
+            'utility_id' => 'required|min:1',
+            'statut_id' => 'required|min:1',
+            'type_id' => 'required|min:1',
+            'place_id' => 'required|min:1',
+            'duree' => 'required|date',
         ]);
+
+        $materiel = new Materiel();
+        $materiel->nom = $request->nom;
+        $materiel->numero_serie = $request->numero_serie;
+        $materiel->token = $request->token;
+        $materiel->utility_id = $request->utility_id;
+        $materiel->statut_id = $request->statut_id;
+        $materiel->type_id = $request->type_id;
+        $materiel->place_id = $request->place_id;
+        $materiel->duree = $request->duree;
+        $materiel->save();
+
+        return redirect()->back()->with(['msg' => "Materiel enregisté avec succès"]);
     }
 
     /**
