c19ba183953028a92f0ae5360af088cfe02ebf06
kevas007 <kevassiobo@gmail.com>
2022-02-15 10:37:23 +0100

    rajou

--- Modules/Inventaire/Resources/assets/components/Inventaire.vue
+++ Modules/Inventaire/Resources/assets/components/Inventaire.vue
@@ -2,6 +2,9 @@
     <div>
         <h1>Inventaire</h1>
         <h2>Liste des produits</h2>
+        <p>
+                <button class="btn btn-primary">Ajouter un produit</button>
+        </p>
     </div>
 </template>
 <script>
