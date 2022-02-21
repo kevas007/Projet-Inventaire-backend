63ade15ab3dcceaec7853bd7cba910a021fcd4c4
kevas007 <kevassiobo@gmail.com>
2022-02-21 09:27:30 +0100

    correction de byg

--- Modules/Inventaire/Resources/assets/js/components/materiel/SearchQr.vue
+++ Modules/Inventaire/Resources/assets/js/components/materiel/SearchQr.vue
@@ -5,12 +5,12 @@
             <b>{{ result }}</b>
         </p>
 
-        <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+        <qrcode-stream   :key="_uid" :camera="camera" @decode="onDecode" @init="onInit"  :track="selected.value">
             <div v-if="validationSuccess" class="validation-success">C'est le bon Matériel</div>
 
-            <div v-if="validationFailure" class="validation-failure">C'est pas le bon Matériel!</div>
+            <div v-if="validationFailure" class="validation-failure">C'est pas le bon Matériel! Relancer le scan</div>
 
-            <div v-if="validationPending" class="validation-pending">Long validation in progress...</div>
+            <div v-if="validationPending" class="validation-pending">Scan en cours...</div>
         </qrcode-stream>
     </div>
 </template>
@@ -29,10 +29,20 @@ export default {
     components: { QrcodeStream },
 
     data() {
+          const options = [
+            { text: "nothing (default)", value: undefined },
+            { text: "outline", value: this.paintOutline },
+            { text: "centered text", value: this.paintCenterText },
+            { text: "bounding box", value: this.paintBoundingBox },
+        ]
+
+        const selected = options[1]
         return {
             isValid: undefined,
             camera: 'auto',
             result: null,
+            selected,
+            options ,
         }
     },
 
@@ -56,6 +66,58 @@ export default {
     },
 
     methods: {
+            paintOutline(detectedCodes, ctx) {
+            for (const detectedCode of detectedCodes) {
+                const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+                ctx.strokeStyle = "green";
+
+                ctx.beginPath();
+                ctx.moveTo(firstPoint.x, firstPoint.y);
+                for (const { x, y } of otherPoints) {
+                    ctx.lineTo(x, y);
+                }
+                ctx.lineTo(firstPoint.x, firstPoint.y);
+                ctx.closePath();
+                ctx.stroke();
+            }
+        },
+
+        paintBoundingBox(detectedCodes, ctx) {
+            for (const detectedCode of detectedCodes) {
+                const { boundingBox: { x, y, width, height } } = detectedCode
+
+                ctx.lineWidth = 2
+                ctx.strokeStyle = '#007bff'
+                ctx.strokeRect(x, y, width, height)
+            }
+        },
+
+        paintCenterText(detectedCodes, ctx) {
+            for (const detectedCode of detectedCodes) {
+                const { boundingBox, rawValue } = detectedCode
+
+                const centerX = boundingBox.x + boundingBox.width / 2
+                const centerY = boundingBox.y + boundingBox.height / 2
+
+                const fontSize = Math.max(12, 50 * boundingBox.width / ctx.canvas.width)
+                console.log(boundingBox.width, ctx.canvas.width)
+
+                ctx.font = `bold ${fontSize}px sans-serif`
+                ctx.textAlign = "center"
+
+                ctx.lineWidth = 3
+                ctx.strokeStyle = '#35495e'
+                ctx.strokeText(detectedCode.rawValue, centerX, centerY)
+
+                ctx.fillStyle = '#5cb984'
+                ctx.fillText(rawValue, centerX, centerY)
+            }
+        },
+
+        logErrors(promise) {
+            promise.catch(console.error)
+        },
 
         onInit(promise) {
             promise
